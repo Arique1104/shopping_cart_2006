3401cdf15af56af66a2eedb5d9b008dfee3d4a89
Arique Aguilar <arique1104@gmail.com>
2020-07-08 10:06:04 -0500

    Passing unit price method

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,11 @@
 class Product
  attr_reader  :category,
-              :name
+              :name,
+              :unit_price
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
+    @unit_price = unit_price
 
   end
 
