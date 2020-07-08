3388ec13d330696a57c8a9af2ead3c915b5e0e2a
Arique Aguilar <arique1104@gmail.com>
2020-07-08 11:08:53 -0500

    Product breakdown test complete

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -129,7 +129,26 @@ class ShoppingCartTest < Minitest::Test
   end
 
   def test_it_can_provide_product_breakdown
-    skip
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    expected = {
+      :meat => [product2],
+      :paper => [product1, product3],
+      :produce => [product4]
+      }
+      
+    assert_equal expected, cart.product_breakdown
   end
 
 
